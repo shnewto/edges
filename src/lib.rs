662f42c7e1d478a66b62555801bf6f85ad6f36d4
salam <salamatbekboev2008@gmail.com>
2024-11-16 04:28:58 +0900

    add: impl trait for convert `Edges` into `Vec`

--- src/lib.rs
+++ src/lib.rs
@@ -182,6 +182,12 @@ impl Edges {
     }
 }
 
+impl From<Edges> for Vec<Vec<UVec2>> {
+    fn from(value: Edges) -> Vec<Vec<UVec2>> {
+        value.image_edges()
+    }
+}
+
 #[cfg(feature = "bevy")]
 impl From<bevy_render::prelude::Image> for Edges {
     fn from(i: bevy_render::prelude::Image) -> Edges {
